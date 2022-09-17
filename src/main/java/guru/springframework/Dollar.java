c4167605e6c0ff7a129b58d9819a2f45638663d7
Ihor <igorok.vin@gmail.com>
2022-09-17 15:22:19 -0600

    getting rid of Franc/Dollar duplication

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -1,9 +1,6 @@
 package guru.springframework;
 
-import java.util.Objects;
-
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -18,16 +15,5 @@ public class Dollar {
         return amount == dollar.amount;
     }*/
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
-    }
 }
--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -2,14 +2,14 @@ package guru.springframework;
 
 import java.util.Objects;
 
-public class Franc {
-    private int amount;
+public class Franc extends Money {
 
     public Franc(int amount) {
         this.amount = amount;
     }
 
     Franc times(int multiplier) {
+
         return new Franc(amount * multiplier);
     }
 
@@ -17,17 +17,4 @@ public class Franc {
         Franc franc = (Dollar) o;
         return amount == franc.amount;
     }*/
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Franc dollar = (Franc) o;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
-    }
 }
